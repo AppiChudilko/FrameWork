86ef85d57bb168b70476da2a4eb824a35fa8cff0
AppiChudilko <channelappi@gmail.com>
2016-08-24 09:02:28 +0300

    v0.0.8
    Add statistic class

--- apFunc.php
+++ apFunc.php
@@ -15,7 +15,7 @@ use \PDO;
 class EnumConst
 {	
 
-	const VERSION = '0.0.7';
+	const VERSION = '0.0.8';
 	/*
 	* ERRORS
 	*/
@@ -34,6 +34,7 @@ class EnumConst
 	*/
 	const USERS = 'a_user';
 	const NEWS = 'a_news';
+	const STATS = 'fw_statistic';
 
 	/*
 	* DATA BASE COLUMN NAME
@@ -45,6 +46,10 @@ class EnumConst
 	const U_LOGIN = 'login';
 	const U_PASSWORD = 'password';
 	const U_TOKEN = 'token';
+
+	const ST_IP = 'ip';
+	const ST_COUNT = 'count';
+	const ST_LAST_CONNECT = 'last_connect';
 }
 
 /**
@@ -692,7 +697,7 @@ class DataBase
 				$this->query->bindvalue($this->paramsColumn[$i].'_val', $this->charsHtmlScript($this->paramsValues[$i]));
 			}
 		}
-
+		
 		$this->query->execute();
 
 		$this->server->log($this->sql, "logs/sql.log");
@@ -732,6 +737,105 @@ class DataBase
 
 		$value = htmlspecialchars(stripslashes($value));
 		return $value;
+	}
+}
+
+/**
+* Statistic
+*/
+class Statistic
+{
+	protected $statsResult;
+
+	protected $clientIp;
+
+	protected $timeNow;
+
+	protected $qb;
+
+	function __construct($qb) {
+
+		$server = new Server;
+		$this->qb = new QueryBuilder();
+		$this->qb = $qb;
+		$this->clientIp = $server->getClientIp();
+		$this->timeNow = $server->unixNow();
+	}
+
+	public function createStatistic() {
+
+		if (empty($_SESSION['time_stats']) || $_SESSION['time_stats'] > $this->timeNow+600) {
+			
+			$this->getClientStats();
+
+			if (!empty($this->statsResult)) {
+				if (date('Y/d', $this->timeNow) != date('Y/d', $this->statsResult[EnumConst::ST_LAST_CONNECT])) {
+					$this->insertClientStats();
+				}
+				else {
+					$this->updateClientStats();
+				}
+			}
+			else {
+				$this->insertClientStats();
+			}
+			$_SESSION['time_stats'] = $this->timeNow;
+		}
+		else {
+			$_SESSION['count_stats']++;
+		}
+	}
+
+	public function getAllStats() {
+		$result = $this->qb
+			->createQueryBuilder(EnumConst::STATS)
+			->selectSql()
+			->executeQuery()
+			->getResult()
+		;
+
+		return $result;
+	}
+
+	protected function getClientStats() {
+
+		$this->statsResult = end($this->qb
+			->createQueryBuilder(EnumConst::STATS)
+			->selectSql()
+			->where(EnumConst::ST_IP." = '".$this->clientIp."'")
+			->executeQuery()
+			->getResult()
+		);
+		return $this;
+	}
+
+	protected function updateClientStats() {
+
+		if (empty($_SESSION['count_stats'])) {
+			$_SESSION['count_stats'] = 1;
+		}
+
+		$this->qb
+			->createQueryBuilder(EnumConst::STATS)
+			->updateSql([EnumConst::ST_COUNT], [$this->statsResult[EnumConst::ST_COUNT] + $_SESSION['count_stats']])
+			->where(EnumConst::ST_IP." = '".$this->clientIp."'")
+			->executeQuery()
+		;
+
+		$_SESSION['count_stats'] = 1;
+
+		return $this;
+	}
+
+	protected function insertClientStats() {
+
+		$_SESSION['count_stats'] = 1;
+		$this->qb
+			->createQueryBuilder(EnumConst::STATS)
+			->insertSql([EnumConst::ST_IP, EnumConst::ST_COUNT, EnumConst::ST_LAST_CONNECT], [$this->clientIp, '1', $this->timeNow])
+			->executeQuery()
+		;
+		return $this;
 	}
 }
 
